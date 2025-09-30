ec7d2a22a9118f330678f92f27e3526615fc07d4
AbhayShaw0125 <abhayshaw0125@gmail.com>
2025-10-01 02:30:59 +0530

    chore(backend): add CORS configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routers import average,emwa
 
 app=FastAPI(title="EWMA Assignment API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"], 
+)
 
 app.include_router(average.router)
 app.include_router(emwa.router)
