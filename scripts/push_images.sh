3a427b897426951819c0565658a1bec427c65124
Abhay Shaw <148813870+AbhayShaw1@users.noreply.github.com>
2025-10-02 13:59:53 +0530

    Update push_images.sh

--- scripts/push_images.sh
+++ scripts/push_images.sh
@@ -13,20 +13,9 @@ set -e  # Exit on first error
 
 REGISTRY="abhayshaw0301"
 
-
-BACKEND_IMAGE="$REGISTRY/takehome-backend"
-FRONTEND_IMAGE="$REGISTRY/takehome-frontend"
-
-################
-# Push backend images
-################
-docker push "$BACKEND_IMAGE-staging:latest"
-docker push "$BACKEND_IMAGE-prod:stable"
-
-################
-# Push frontend images
-################
-docker push "$FRONTEND_IMAGE-staging:latest"
-docker push "$FRONTEND_IMAGE-prod:stable"
+docker push "${REGISTRY}-staging:latest"
+docker push "${REGISTRY}-prod:stable"
+docker push "${REGISTRY}-frontend-staging:latest"
+docker push "${REGISTRY}-frontend-prod:stable"
 
 echo "✅ All Docker images pushed successfully to Docker Hub"
